4d13d4694a4ece34af7f450247d80ea6f6ba90ad
shian009 <shiankmaheshwari14@gmail.com>
2020-03-29 18:20:23 +0530

    completed

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
 const request = require('request');
+const bp = require('body-parser');
+
 const PORT = process.env.PORT || 5000;
+
+
+//use body parser middleware
+app.use(bp.urlencoded({extended:false}));
 
 
 //API key Publishable API Token: pk_09999b51fa1e49569e5ccd99f02e4a75
 //call api function
-function call_api(finished){ 
-request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_09999b51fa1e49569e5ccd99f02e4a75', {json:true},(err,res,body)=>{
+function call_api(finished,ticker){ 
+request('https://cloud.iexapis.com/stable/stock/'+ ticker +'/quote?token=pk_09999b51fa1e49569e5ccd99f02e4a75', {json:true},(err,res,body)=>{
     if (err) {
         return console.log(err);
     }
@@ -17,7 +23,7 @@ request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_09999b51fa1e49
         finished(body);
     }
 });
-}
+};
 
 //set handlebars MIddleware
 app.engine('handlebars',exphbs());
@@ -29,12 +35,17 @@ app.get('/',function(req,res){
         res.render('home',{
             stuff: done 
         });
-    });  
+    },"fb");  
 });
-//static folder
-app.use(express.static(path.join(__dirname,'public')));
 
-
+//set handlebars POST routes
+app.post('/',function(req,res){
+    call_api(function(done){
+        res.render('home',{
+            stuff: done
+        });
+    },req.body.stockchecker);  
+});
 
 app.listen(PORT, ()=>{
     console.log('Server listening @ '+PORT);
